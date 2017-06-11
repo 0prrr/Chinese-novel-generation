b355e855277dfc2e8e7a26f89962464d184a6a20
darkie <darkiexu329@gmail.com>
2017-06-12 03:56:40 +0800

    Modify helper.py.

--- helper.py
+++ helper.py
@@ -5,11 +5,6 @@ import os
 
 
 def load_text(path):
-
-    '''
-    load raw text data
-    '''
-
     input_file = os.path.join(path)
 
     with open(input_file, 'r') as f:
@@ -19,22 +14,15 @@ def load_text(path):
 
 
 def preprocess_and_save_data(text, token_lookup, create_lookup_tables):
-    #text = load_text(dataset_path)
-
     token_dict = token_lookup()
     
     for key, token in token_dict.items():
-        text = text.replace(key, ' {} '.format(token))
+        text = text.replace(key, '{}'.format(token))
 
     text = list(text)
     
-    print('preprocess text len:{}'.format(len(text)))
-
     vocab_to_int, int_to_vocab = create_lookup_tables(text)
     int_text = [vocab_to_int[word] for word in text]
-    
-    print('preprocess int_text len:{}'.format(len(int_text)))
-    
     
     pickle.dump((int_text, vocab_to_int, int_to_vocab, token_dict), open('preprocess.p', 'wb'))
 
